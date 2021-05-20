4fc2f570e3e56436d5363f7fe0ce87448c1cd16a
jamesdidit72 <jamesjbarton72@gmail.com>
2021-05-20 16:04:19 +0100

    math functions

--- Functions.py
+++ Functions.py
@@ -1,11 +1,44 @@
 # function
 # syntax def name():
 
+#
+# def greeting(name):
+#     print(f'Hello {name}')
+#     print('Welcome to Cyber Security')
+#
+#
+# user = input('What is your name? ')
+# greeting(user)
 
-def greeting(name):
-    print(f'Hello {name}')
-    print('Welcome to Cyber Security')
+# A function that takes 2 args (arguments) as ints
 
 
-user = input('What is your name? ')
-greeting(user)
+def add(value_1, value_2):
+    return value_1 + value_2
+
+
+print(add(5, 5))
+
+
+def subtract(value_1, value_2):
+    return value_1 - value_2
+
+
+print(subtract(5, 5))
+
+def multiply(value_1, value_2):
+    return value_1 * value_2
+
+
+print(multiply(5, 5))
+
+
+def divide(value_1, value_2):
+    return value_1 / value_2
+
+
+print(divide(5, 5))
+
+
+# def percentage():
+#
--- README.md
+++ README.md
@@ -11,3 +11,33 @@ def greeting(name):
 user = input('What is your name? ')
 greeting(user)
 ```
+### math functions
+
+```python
+def add(value_1, value_2):
+    return value_1 + value_2
+
+
+print(add(5, 5))
+
+
+def subtract(value_1, value_2):
+    return value_1 - value_2
+
+
+print(subtract(5, 5))
+
+def multiply(value_1, value_2):
+    return value_1 * value_2
+
+
+print(multiply(5, 5))
+
+
+def divide(value_1, value_2):
+    return value_1 / value_2
+
+
+print(divide(5, 5))
+
+```
